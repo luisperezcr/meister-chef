fe1415a0015e38518c6ac044fe4f7c97a9f874ef
Luis Pérez <luis.perez@geekdacity.com>
2020-04-04 14:31:52 -0600

    feat(app): add & configure firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
+import {environment} from '../environments/environment';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    AngularFireModule.initializeApp(environment.firebase, 'meister-chef')
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
